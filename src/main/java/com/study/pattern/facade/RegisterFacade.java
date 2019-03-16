79ef23702ad2fabc3fbeab582004fca719b9b6c6
david <2433liushuai@163.com>
2019-03-16 08:42:05 +0800

    外观模式

---
+++ src/main/java/com/study/pattern/facade/RegisterFacade.java
@@ -0,0 +1,18 @@
+package com.study.pattern.facade;
+
+/**
+ * 办理注册公司流程的门面对象
+ * <p>
+ * 迪米特法则
+ * 让别人调用的时候，尽量简单
+ * <p>
+ * 各种工具类
+ * <p>
+ * 没啥写的。。。。
+ */
+public class RegisterFacade {
+
+    public void register() {
+        //对其他对象的封装，类似server对dao的封装
+    }
+}
